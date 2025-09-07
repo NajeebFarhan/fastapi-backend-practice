52c94b4db12d8591ff3d73d52671e438eaee1a36
NajeebFarhan <najeebfarhan6345@gmail.com>
2025-09-07 20:54:08 +0530

    minor change

--- util/token.py
+++ util/token.py
@@ -1,14 +1,20 @@
 from jose import jwt, JWTError
 from fastapi import HTTPException
 from os import environ
+from datetime import datetime, timezone, timedelta
 
 SECRET_KEY: str = environ.get("SECRET_KEY") or ""
 ALGORITHM: str = environ.get("ALGORITHM") or ""
+EXPIRY: int = int(environ.get("EXPIRY") or 15)
 
 print(SECRET_KEY, ALGORITHM)
 
-def create_token(sub: str, expiry: int=15):
-    to_encode = {"sub": sub } #, "exp": datetime.now(timezone.utc) + timedelta(minutes=expiry)}
+def create_token(sub: str, expiry: int | None = None):
+    
+    if not expiry:
+        expiry = EXPIRY
+    
+    to_encode = {"sub": sub, "exp": datetime.now(timezone.utc) + timedelta(minutes=expiry)}
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
